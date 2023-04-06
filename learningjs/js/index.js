df2500f696258258b11f1096564ab1ac880fcfd0
Victor Mota <106634121+voctif@users.noreply.github.com>
2023-04-05 21:14:19 -0300

    sorteador arrumado

--- learningjs/js/index.js
+++ learningjs/js/index.js
@@ -23,9 +23,12 @@ function conta(){
 
 function sortear(){
     min = document.getElementById("mandar").value;
+    min2 = parseInt(min);
     max = document.getElementById("mandar2").value;
+    max2 = parseInt(max);
 
-    var num = Math.floor((Math.random() * (max-min + 1)) +min);
+
+    var num = Math.floor((Math.random() * (max2-min2 + 1)) +min2);
     document.getElementById('sor').innerHTML = num;
 
 }
