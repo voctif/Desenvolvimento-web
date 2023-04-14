03f79162ee28801c97a940775bfb4125f76938f9
Victor Mota <106634121+voctif@users.noreply.github.com>
2023-04-14 11:24:50 -0300

    feature: bootstrap

--- learningjs/js/index.js
+++ learningjs/js/index.js
@@ -1,26 +1,3 @@
-function tabuada(){
-
-    let num = document.getElementById('tabuada').value;
-    let divisoria = document.getElementById('result');
-    divisoria.innerHTML = `<p>a tabuada é ${num} </p>`;
-    for(var i = 1; i <=10; i++){
-        let calc = (num * i);
-        const node = document.createElement("p");
-        const text = document.createTextNode(`${num} x ${i} : ${calc} `);
-        node.appendChild(text);
-        divisoria.appendChild(node);
-  
-    }
-}
-
-let cliques =0;
-
-function conta(){
-    cliques++;
-    document.getElementById('cliqu').innerHTML = cliques;
-
-}
-
 function sortear(){
     min = document.getElementById("mandar").value;
     min2 = parseInt(min);
